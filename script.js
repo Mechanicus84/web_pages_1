dd058801e395d7ef62fce9024fc04e1cc1c4e0d5
Mechanicus84 <ianacklam@gmail.com>
2025-09-15 16:56:16 +0100

    add connection to collect_dates

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-function addNumbers() {
-  const num1 = parseFloat(document.getElementById("num1").value) || 0;
-  const num2 = parseFloat(document.getElementById("num2").value) || 0;
-  const sum = num1 + num2;
-  document.getElementById("result").innerText = "Result: " + sum;
+function addNumbers() 
+{
+    const num1 = parseFloat(document.getElementById("num1").value) || 0;
+    const num2 = parseFloat(document.getElementById("num2").value) || 0;
+    const sum = num1 + num2;
+    document.getElementById("result").innerText = "Result: " + sum;
+}
+
+function collect_dates () 
+{
+    console.log("\n --- collect_dates start --- ")
+    var start_date = document.getElementById("start_date").value;
+    var end_date = document.getElementById("end_date").value;
+    var date_values = calculate_dates(start_date, end_date);
+
+    console.log("\n collect_dates end --- ")
+
 }
 
 function calculate_dates(start_date,end_date) 
 {
-    	console.log("\n --- calculate_days start --- ")
+    console.log("\n --- calculate_days start --- ")
 
-    	var time_diff = end_date - start_date;
-    	var day_milliseconds = 86400000
-    	var days_diff = time_diff / day_milliseconds;
-    	var weeks_diff = days_diff / 7
+    var time_diff = end_date - start_date;
+    var day_milliseconds = 86400000
+    var days_diff = time_diff / day_milliseconds;
+    var weeks_diff = days_diff / 7
 
-    	weeks_diff = parseFloat(weeks_diff).toFixed(2)
+    weeks_diff = parseFloat(weeks_diff).toFixed(2)
 
-    	console.log(`Start date : ${start_date}`)
-    	console.log(`End date : ${end_date}`)
-    	console.log(`Days : ${days_diff} (${weeks_diff} weeks)`)
+    console.log(`Start date : ${start_date}`)
+    console.log(`End date : ${end_date}`)
+    console.log(`Days : ${days_diff} (${weeks_diff} weeks)`)
 
-    	const return_values = {
-        	days : days_diff,
-        	weeks : weeks_diff,
-    	}
-    	console.log(" --- calculate_days end --- ")
-    	return return_values
+    const return_values = 
+    {
+        days : days_diff,
+        weeks : weeks_diff,
+    }
+    console.log(" --- calculate_days end --- ")
+    return return_values
 }
 
 function calculate_weight_stlb_kg (stones,pounds)
